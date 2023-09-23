b3c909981eb50715ee9c65dc73ec603bdf8fa570
Matias Navarro <matiasg.developer@gmail.com>
2023-09-23 01:20:11 -0300

    bouncing the ball

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
+let requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+
+let cancelAnimationFrame =
+  window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+
+let animationFrame;
 let canvas = document.getElementById("canvas");
 let c = canvas.getContext("2d");
 
 canvas.width = 600;
 canvas.height = 400;
-
-c.fillStyle = "rgba(255, 0, 0, 0.5)";
-
-// rectangles
-c.fillRect(100, 100, 200, 100);
-c.fillRect(400, 140, 200, 100);
-
-// lines
-c.strokeStyle = "#f94";
-c.beginPath();
-c.moveTo(50, 20);
-c.lineTo(50, 95);
-c.lineTo(100, 20);
-c.stroke();
-
-// arcs / circles
-
-// c.strokeStyle = "#000";
-// c.beginPath();
-// c.arc(200, 350, 40, Math.PI / 2, Math.PI * 2, true);
-// c.stroke();
-
 c.strokeStyle = "#000";
 
-for (let i = 0; i < 12; i++) {
-  let x = 100 + 100 * i;
-  let y = 200;
+let velocity = 4;
+let radius = 40;
+let dx = velocity;
+let dy = velocity;
+
+let x = 100;
+let y = 100;
+
+function moveCircle() {
+  c.clearRect(0, 0, 600, 400);
   c.beginPath();
-  c.arc(x, y, 45, 0, Math.PI * 2);
+  c.arc(x, y, radius, 0, Math.PI * 2);
   c.stroke();
+
+  if (x + radius > canvas.width || x - radius < 0) {
+    dx = -dx;
+  }
+
+  if (y + radius > canvas.height || y - radius < 0) {
+    dy = -dy;
+  }
+
+  x += dx;
+  y += dy;
+
+  animationFrame = requestAnimationFrame(moveCircle);
 }
+
+moveCircle();
